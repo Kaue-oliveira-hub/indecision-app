b0e8dcab866e97ee646a400ccd3b3a5f7651958e
Kaue-oliveira-hub <kaueoliveiramcz@gmail.com>
2025-02-25 20:06:21 +0100

    Pantalla mensaje de chat

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
+
 <template>
+<IndecisionView />
+
+
+
+</template>
+
+
+
+<script lang="ts" setup>
+import IndecisionView from './views/indecisionView.vue';
+
+ 
+
+
+
+
+</script>
+
+
+<!--<template>
     <h1 class="text-5xl font-semibold">Mi primera App de Vue</h1>
-   
+   <h2>Hello World!</h2>
     <MeusNumerator :valorem="7"/>
     <MeusNumeratorScript :valorem="10"/>
     <MeusNumeratorScript2 :valorem="15"/>
@@ -13,4 +34,5 @@
    
    
   </script>
-   
+   -->
+
