d8f2aabce7d46e9b406ad32507e560e623f925ff
Kaue-oliveira-hub <kaueoliveiramcz@gmail.com>
2025-02-25 20:18:43 +0100

    Componentes del chat

---
+++ src/components/chat/ChatNuntii.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="p-4 flex-1 overflow-y-auto">
+        <div class="flex- flex-col space-y-2">
+            <div class="flex justify-end">
+                <div class="bg-blue-200 text-black p-4 max-w-xs rounded-lg">
+        Hi, What about your day?
+         </div>
+</div>
+
+<div class="flex justify-start">
+    <div class="bg-gray-300 text-black p-4 max-w-xs rounded-lg">
+        That was great. What about your day? 
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
