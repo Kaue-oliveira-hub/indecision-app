10a87f0896c3410a009852df7c0c1c24525d3175
Kaue-oliveira-hub <kaueoliveiramcz@gmail.com>
2025-02-25 20:39:59 +0100

    app yes no

--- src/components/chat/ChatNuntii.vue
+++ src/components/chat/ChatNuntii.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="p-4 flex-1 overflow-y-auto">
         <div class="flex- flex-col space-y-2">
+           
+           <!-- 
             <div class="flex justify-end">
                 <div class="bg-blue-200 text-black p-4 max-w-xs rounded-lg">
         Hi, What about your day?
@@ -12,7 +14,16 @@
         That was great. What about your day? 
             </div>
         </div>
+
+-->
+<ChatBullula :meus-est="true" nuntius="let's get out soon?"/>
     </div>
 </div>
 
 </template>
+
+<script lang="ts" setup>
+
+import ChatBullula from './ChatBullula.vue';
+
+</script>
